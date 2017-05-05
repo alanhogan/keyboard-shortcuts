33a46507ec7491a45690425b4023a0f44ec116c3
Alan Hogan <contact@alanhogan.com>
2017-05-04 20:14:28 -0700

    Add first version that can handle mac and win formatting of a Bold command

---
+++ src/index.js
@@ -0,0 +1,79 @@
+function tokensForWin(tokens) {
+  return tokens.map((token) => {
+    if (token === 'CmdXorCtrl') {
+      return 'Ctrl'
+    } else if (token.length === 1) {
+      return token.toUpperCase();
+    } else {
+      return token;
+    }
+  })
+}
+
+function tokensForMac(tokens) {
+  return tokens.map((token) => {
+    if (token === 'CmdXorCtrl') {
+      return '⌘'
+    } else if (token.length === 1) {
+      return token.toUpperCase();
+    } else {
+      return token;
+    }
+  })
+}
+
+class Shortcuts {
+  constructor(dict, isMac = false) {
+    this.dict = dict;
+    this.mac = !! isMac;
+  }
+
+  set mac(isMac) {
+    this.isMac = !! isMac;
+  }
+  get mac () {
+    return this.isMac;
+  }
+
+  describeAsString(shortcut) {
+    if (this.mac) {
+      return this.describeAsMacString(shortcut);
+    } else {
+      return this.describeAsWinString(shortcut);
+    }
+  }
+
+  describeAsWinString(shortcut) {
+    this.requireShortcut(shortcut);
+
+    let tokens = tokensForWin(this.dict[shortcut]);
+    const lastToken = tokens[tokens.length - 1];
+
+    if (["+", "-"].indexOf(lastToken) > -1) {
+      // Special case. We want "Ctrl +" instead of "Ctrl++"
+      tokens.pop();
+      return `${tokens.join("+")} ${lastToken}`
+    }
+
+    return tokens.join("+");
+  }
+
+  describeAsMacString(shortcut) {
+    this.requireShortcut(shortcut);
+
+    return tokensForMac(this.dict[shortcut]).join("");
+  }
+
+  requireShortcut(shortcut) {
+    if (!Object.prototype.hasOwnProperty.call(this.dict, shortcut)) {
+      throw new Error(`Shortcut “${shortcut}” does not exist`);
+    }
+  }
+}
+
+
+function shortcuts (dict, mac) {
+  return new Shortcuts(dict, mac);
+}
+
+module.exports = shortcuts;
---
+++ test/format.test.js
@@ -0,0 +1,28 @@
+const shortcuts = require('../src/index.js');
+
+const shortcutsDict = {
+  bold: ["CmdXorCtrl", "b"],
+  zoom_in: ["Ctrl", "+"],
+  foo: ["Ctrl", "Shift", "c"]
+};
+
+const winShortcuts = shortcuts(shortcutsDict);
+
+const macShortcuts = shortcuts(shortcutsDict, true);
+
+
+test("defaults to not mac", () => {
+    expect(winShortcuts.mac).toBe(false);
+});
+
+test("bold is right on Windows", () => {
+    expect(winShortcuts.describeAsString("bold")).toBe("Ctrl+B");
+});
+
+test("thinks it's mac if you say so", () => {
+  expect(macShortcuts.mac).toBe(true);
+});
+
+test("bold is right on Mac", () => {
+    expect(macShortcuts.describeAsString("bold")).toBe("⌘B");
+});
